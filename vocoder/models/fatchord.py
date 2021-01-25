1b59254c417fdf9706f9fd92d2c2e8bb773c8251
Aradhya-Tripathi <at8029@srmist.edu.in>
2021-01-25 21:37:40 +0530

    fixed imports

--- vocoder/models/fatchord.py
+++ vocoder/models/fatchord.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from utils.distribution import sample_from_discretized_mix_logistic
-from utils.display import *
-from utils.dsp import *
+from vocoder.distribution import sample_from_discretized_mix_logistic
+from vocoder.display import *
+from vocoder.audio import *
 import os
 import numpy as np
 from pathlib import Path
